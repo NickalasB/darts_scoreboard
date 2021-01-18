97f4b8f3b4329ee75c88fea122576ab29d9b75cd
Nick Bradshaw <nickalasb@gmail.com>
2021-01-18 15:01:22 -0800

    Add some animation fixes

--- lib/ui/score_column.dart
+++ lib/ui/score_column.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:darts_scoreboard/ui/player_name.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -37,7 +40,6 @@ class _ScoreColumnState extends State<ScoreColumn> {
                 (p) => ScoreBox(p, (score) {
                   setState(() {
                     totalScore = totalScore + score;
-                    print('Nick total $totalScore');
                   });
 
                   return score;
@@ -60,13 +62,49 @@ class ScoreBox extends StatefulWidget {
   _ScoreBoxState createState() => _ScoreBoxState();
 }
 
-class _ScoreBoxState extends State<ScoreBox> {
+class _ScoreBoxState extends State<ScoreBox>
+    with SingleTickerProviderStateMixin {
   int hits = 0;
   int scoredPoints = 0;
+  double _progress = 0.0;
+  Paint pointPainter;
+
+  final crossTween = Tween(begin: 1.0, end: 0.0);
+  // TODO(me): The circle needs a 0 - 1 tween
+  final circleTween = Tween(begin: 0.0, end: 1.0);
+
+  Animation<double> animation1;
+
+  AnimationController controller1;
 
   @override
   void initState() {
     super.initState();
+    controller1 = AnimationController(
+        duration: Duration(milliseconds: 3000), vsync: this);
+
+    animation1 = crossTween.animate(controller1)
+      ..addListener(() {
+        setState(() {
+          _progress = animation1.value;
+        });
+      });
+
+    pointPainter = Paint()
+      ..color = Colors.green
+      ..strokeWidth = 5
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+  }
+
+  @override
+  void dispose() {
+    controller1.dispose();
+    super.dispose();
+  }
+
+  void startAnimation() {
+    controller1.forward();
   }
 
   @override
@@ -78,36 +116,22 @@ class _ScoreBoxState extends State<ScoreBox> {
             hits++;
             if (hits >= 4) {
               widget.score(widget.point);
+            } else {
+              startAnimation();
             }
           });
         },
         child: CustomPaint(
-          painter: CricketPointPainter(hits),
+          foregroundPainter: CricketPointPainter(pointPainter, hits, _progress),
           child: DecoratedBox(
             decoration: BoxDecoration(
               border: Border.all(width: 2),
               borderRadius: BorderRadius.circular(4),
             ),
             child: Center(
-              child: Stack(
-                children: <Widget>[
-                  Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        widget.point.toString(),
-                        style: TextStyle(fontSize: 48),
-                      )),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        hits <= 3 ? hits.toString() : '3',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                ],
+              child: Text(
+                widget.point == 25 ? 'B' : widget.point.toString(),
+                style: TextStyle(fontSize: 48),
               ),
             ),
           ),
@@ -118,40 +142,55 @@ class _ScoreBoxState extends State<ScoreBox> {
 }
 
 class CricketPointPainter extends CustomPainter {
-  const CricketPointPainter(this.hits);
+  const CricketPointPainter(this.pointPainter, this.hits, this.progress);
 
+  final Paint pointPainter;
   final int hits;
+  final double progress;
 
   @override
   void paint(Canvas canvas, Size size) {
-    final pointPainter = Paint()
-      ..color = Colors.green
-      ..strokeWidth = 5
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round;
+    var rect = Rect.fromCircle(
+        center: Offset(size.width / 2, size.height / 2),
+        radius: (size.height / 2) * .90);
 
-    final firstStartingPoint = Offset(0, 0);
-    final firstEndingPoint = Offset(size.width, size.height);
+    final firstHitPath = Path()..lineTo(0, 0)..lineTo(size.width, size.height);
 
-    final secondStartingPoint = Offset(size.width, 0);
-    final secondEndingPoint = Offset(0, size.height);
+    final secondHitPath = Path()
+      ..moveTo(size.width, 0)
+      ..lineTo(0, size.height);
 
-    final center = Offset(size.width / 2, size.height / 2);
+    final thirdHitPath = Path()
+      ..addOval(Rect.fromCircle(
+          center: Offset(size.width / 2, size.height / 2),
+          radius: (size.height / 2) * .90));
 
     if (hits == 1) {
-      canvas.drawLine(firstStartingPoint, firstEndingPoint, pointPainter);
+      canvas.drawLine(
+        Offset(0.0, 0.0),
+        Offset(size.width - size.width * progress,
+            size.height - size.height * progress),
+        pointPainter,
+      );
     } else if (hits == 2) {
-      canvas.drawLine(firstStartingPoint, firstEndingPoint, pointPainter);
-      canvas.drawLine(secondStartingPoint, secondEndingPoint, pointPainter);
+      canvas.drawPath(firstHitPath, pointPainter);
+      canvas.drawLine(
+        Offset(size.width, 0.0),
+        Offset(size.width * progress, size.height - size.height * progress),
+        pointPainter,
+      );
     } else if (hits >= 3) {
-      canvas.drawLine(firstStartingPoint, firstEndingPoint, pointPainter);
-      canvas.drawLine(secondStartingPoint, secondEndingPoint, pointPainter);
-      canvas.drawCircle(center, (size.height / 2) * .95, pointPainter);
+      canvas.drawPath(firstHitPath, pointPainter);
+      canvas.drawPath(secondHitPath, pointPainter);
+
+      canvas.drawArc(rect, -pi / 2, pi * 2 * progress, false, pointPainter);
+
+      // canvas.drawPath(thirdHitPath, pointPainter);
     }
   }
 
   @override
   bool shouldRepaint(CricketPointPainter oldDelegate) {
-    return false;
+    return oldDelegate.progress != progress;
   }
 }
