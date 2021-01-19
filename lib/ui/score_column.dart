c0366acc6f1d188b6315e5762b8a0a26cb47d193
Nick Bradshaw <nickalasb@gmail.com>
2021-01-18 21:09:14 -0800

    Correct animations

--- lib/ui/score_column.dart
+++ lib/ui/score_column.dart
@@ -62,33 +62,31 @@ class ScoreBox extends StatefulWidget {
   _ScoreBoxState createState() => _ScoreBoxState();
 }
 
-class _ScoreBoxState extends State<ScoreBox>
-    with SingleTickerProviderStateMixin {
+class _ScoreBoxState extends State<ScoreBox> with TickerProviderStateMixin {
   int hits = 0;
   int scoredPoints = 0;
-  double _progress = 0.0;
   Paint pointPainter;
 
-  final crossTween = Tween(begin: 1.0, end: 0.0);
-  // TODO(me): The circle needs a 0 - 1 tween
-  final circleTween = Tween(begin: 0.0, end: 1.0);
+  final animationDuration = Duration(milliseconds: 250);
+  double _progress = 0.0;
+  double _progress2 = 0.0;
+  double _progress3 = 0.0;
 
   Animation<double> animation1;
+  Animation<double> animation2;
+  Animation<double> animation3;
 
   AnimationController controller1;
+  AnimationController controller2;
+  AnimationController controller3;
+
+  final lineTween = Tween(begin: 1.0, end: 0.0);
+  final circleTween = Tween(begin: 0.0, end: 1.0);
 
   @override
   void initState() {
     super.initState();
-    controller1 = AnimationController(
-        duration: Duration(milliseconds: 3000), vsync: this);
-
-    animation1 = crossTween.animate(controller1)
-      ..addListener(() {
-        setState(() {
-          _progress = animation1.value;
-        });
-      });
+    initializeAnimations();
 
     pointPainter = Paint()
       ..color = Colors.green
@@ -97,14 +95,51 @@ class _ScoreBoxState extends State<ScoreBox>
       ..strokeCap = StrokeCap.round;
   }
 
+  void initializeAnimations() {
+    controller1 = AnimationController(duration: animationDuration, vsync: this);
+    controller2 = AnimationController(duration: animationDuration, vsync: this);
+    controller3 = AnimationController(duration: animationDuration, vsync: this);
+
+    animation1 = lineTween.animate(controller1)
+      ..addListener(() {
+        setState(() {
+          _progress = animation1.value;
+        });
+      });
+
+    animation2 = lineTween.animate(controller2)
+      ..addListener(() {
+        setState(() {
+          _progress2 = animation2.value;
+        });
+      });
+
+    animation3 = circleTween.animate(controller3)
+      ..addListener(() {
+        setState(() {
+          _progress3 = animation3.value;
+        });
+      });
+  }
+
   @override
   void dispose() {
     controller1.dispose();
+    controller2.dispose();
+    controller3.dispose();
     super.dispose();
   }
 
-  void startAnimation() {
-    controller1.forward();
+  void startAnimation(int hits) {
+    if (hits == 1) {
+      controller1.forward();
+    }
+    if (hits == 2) {
+      controller2.forward();
+    }
+    if (hits == 3) {
+      controller3.forward();
+    }
   }
 
   @override
@@ -117,12 +152,18 @@ class _ScoreBoxState extends State<ScoreBox>
             if (hits >= 4) {
               widget.score(widget.point);
             } else {
-              startAnimation();
+              startAnimation(hits);
             }
           });
         },
         child: CustomPaint(
-          foregroundPainter: CricketPointPainter(pointPainter, hits, _progress),
+          painter: CricketPointPainter(
+            pointPainter,
+            hits,
+            _progress,
+            _progress2,
+            _progress3,
+          ),
           child: DecoratedBox(
             decoration: BoxDecoration(
               border: Border.all(width: 2),
@@ -142,28 +183,27 @@ class _ScoreBoxState extends State<ScoreBox>
 }
 
 class CricketPointPainter extends CustomPainter {
-  const CricketPointPainter(this.pointPainter, this.hits, this.progress);
+  const CricketPointPainter(
+    this.pointPainter,
+    this.hits,
+    this.progress,
+    this.progress2,
+    this.progress3,
+  );
 
   final Paint pointPainter;
   final int hits;
   final double progress;
+  final double progress2;
+  final double progress3;
 
   @override
   void paint(Canvas canvas, Size size) {
-    var rect = Rect.fromCircle(
-        center: Offset(size.width / 2, size.height / 2),
-        radius: (size.height / 2) * .90);
-
     final firstHitPath = Path()..lineTo(0, 0)..lineTo(size.width, size.height);
 
     final secondHitPath = Path()
       ..moveTo(size.width, 0)
       ..lineTo(0, size.height);
-
-    final thirdHitPath = Path()
-      ..addOval(Rect.fromCircle(
-          center: Offset(size.width / 2, size.height / 2),
-          radius: (size.height / 2) * .90));
 
     if (hits == 1) {
       canvas.drawLine(
@@ -176,21 +216,26 @@ class CricketPointPainter extends CustomPainter {
       canvas.drawPath(firstHitPath, pointPainter);
       canvas.drawLine(
         Offset(size.width, 0.0),
-        Offset(size.width * progress, size.height - size.height * progress),
+        Offset(size.width * progress2, size.height - size.height * progress2),
         pointPainter,
       );
     } else if (hits >= 3) {
       canvas.drawPath(firstHitPath, pointPainter);
       canvas.drawPath(secondHitPath, pointPainter);
 
-      canvas.drawArc(rect, -pi / 2, pi * 2 * progress, false, pointPainter);
+      var rect = Rect.fromCircle(
+        center: Offset(size.width / 2, size.height / 2),
+        radius: (size.height / 2) * .90,
+      );
 
-      // canvas.drawPath(thirdHitPath, pointPainter);
+      canvas.drawArc(rect, -pi / 2, pi * 2 * progress3, false, pointPainter);
     }
   }
 
   @override
   bool shouldRepaint(CricketPointPainter oldDelegate) {
-    return oldDelegate.progress != progress;
+    return oldDelegate.progress != progress ||
+        oldDelegate.progress2 != progress2 ||
+        oldDelegate.progress3 != progress3;
   }
 }
